c9778efa4c9c609cb591b1f923ec5448c6f7de44
Silentkaliber <calibretto@verizon.net>
2015-11-30 20:17:55 -0600

    modded

--- myJSfile.js
+++ myJSfile.js
@@ -2,22 +2,66 @@
 		{
 			var hexy;
 			hexy = document.getElementById("hexNum").value;
-			var hexy2 = hexy.toLowerCase();
-			hexy2 = hexy2.replace(/0/g,"0000");
-			hexy2 = hexy2.replace(/1/g,"0001");
-			hexy2 = hexy2.replace(/2/g,"0010");
-			hexy2 = hexy2.replace(/3/g,"0011");
-			hexy2 = hexy2.replace(/4/g,"0100");
-			hexy2 = hexy2.replace(/5/g,"0101");
-			hexy2 = hexy2.replace(/6/g,"0110");
-			hexy2 = hexy2.replace(/7/g,"0111");
-			hexy2 = hexy2.replace(/8/g,"1000");
-			hexy2 = hexy2.replace(/9/g,"1001");
-			hexy2 = hexy2.replace(/a/g,"1010");
-			hexy2 = hexy2.replace(/b/g,"1011");
-			hexy2 = hexy2.replace(/c/g,"1100");
-			hexy2 = hexy2.replace(/d/g,"1101");
-			hexy2 = hexy2.replace(/e/g,"1110");
-			hexy2 = hexy2.replace(/f/g,"1111");
-			document.getElementById("return").innerHTML = hexy2;
+			hexy = hexy.toLowerCase();
+			hexy = hexy.replace(/0/g,"0000");
+			hexy = hexy.replace(/1/g,"0001");
+			hexy = hexy.replace(/2/g,"0010");
+			hexy = hexy.replace(/3/g,"0011");
+			hexy = hexy.replace(/4/g,"0100");
+			hexy = hexy.replace(/5/g,"0101");
+			hexy = hexy.replace(/6/g,"0110");
+			hexy = hexy.replace(/7/g,"0111");
+			hexy = hexy.replace(/8/g,"1000");
+			hexy = hexy.replace(/9/g,"1001");
+			hexy = hexy.replace(/a/g,"1010");
+			hexy = hexy.replace(/b/g,"1011");
+			hexy = hexy.replace(/c/g,"1100");
+			hexy = hexy.replace(/d/g,"1101");
+			hexy = hexy.replace(/e/g,"1110");
+			hexy = hexy.replace(/f/g,"1111");
+			document.getElementById("returnB").innerHTML = hexy;
+		}
+
+
+		function make10()
+		{
+			var hexy;
+			hexy = document.getElementById("hexNum").value;
+			hexy = hexy.toLowerCase();
+			hexy = hexy.replace(/0/g,"0000");
+			hexy = hexy.replace(/1/g,"0001");
+			hexy = hexy.replace(/2/g,"0010");
+			hexy = hexy.replace(/3/g,"0011");
+			hexy = hexy.replace(/4/g,"0100");
+			hexy = hexy.replace(/5/g,"0101");
+			hexy = hexy.replace(/6/g,"0110");
+			hexy = hexy.replace(/7/g,"0111");
+			hexy = hexy.replace(/8/g,"1000");
+			hexy = hexy.replace(/9/g,"1001");
+			hexy = hexy.replace(/a/g,"1010");
+			hexy = hexy.replace(/b/g,"1011");
+			hexy = hexy.replace(/c/g,"1100");
+			hexy = hexy.replace(/d/g,"1101");
+			hexy = hexy.replace(/e/g,"1110");
+			hexy = hexy.replace(/f/g,"1111");
+
+			var sizeOfHexy = hexy.length;
+
+			var total = 0;
+			var counter = 0;
+
+			--sizeOfHexy;
+			
+			while(sizeOfHexy >= 0)
+			{
+				if(hexy.charAt(sizeOfHexy) === "1")
+				{
+					total += Math.pow(2,counter);
+				}
+				--sizeOfHexy;
+				counter++;
+			}
+			
+
+			document.getElementById("returnD").innerHTML = total;
 		}
